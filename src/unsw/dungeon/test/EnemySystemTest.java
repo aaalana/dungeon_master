44c2e43468e1cb21f85797d5e6c6cbf653176c91
Alana Hua <z5209503@ad.unsw.edu.au>
2019-11-02 22:47:20 +1100

    made enemy system test

---
+++ src/unsw/dungeon/test/EnemySystemTest.java
@@ -0,0 +1,41 @@
+package unsw.dungeon.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import com.sun.tools.javac.util.List;
+
+import unsw.dungeon.*;
+
+class EnemySystemTest {
+
+	@Test
+	void testGetEnemies() {
+		EnemySystem sys = new EnemySystem();
+		Dungeon dungeon = new Dungeon(18,16);
+		Enemy e1 = new Enemy(dungeon, 2, 3);
+		Enemy e2 = new Enemy(dungeon, 8, 3);
+		Enemy e3 = new Enemy(dungeon, 8, 7);
+		
+		ArrayList<Entity> enemies = new ArrayList<Entity>();
+		
+		// empty list
+		assertEquals(enemies, sys.getEnemies());
+		
+		// has enemies
+		enemies.add(e1);
+		enemies.add(e2);
+		
+		sys.addEnemy(e1);
+		sys.addEnemy(e2);
+		assertEquals(enemies, sys.getEnemies());
+		
+		// different lists
+		enemies.add(e3);
+		assertNotSame(enemies, sys.getEnemies());
+	}
+
+}
