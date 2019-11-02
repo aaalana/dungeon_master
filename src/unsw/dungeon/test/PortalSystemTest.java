f552c55f281c3a234d3dea3845d28c2d8feb6601
Alana Hua <z5209503@ad.unsw.edu.au>
2019-11-02 23:05:11 +1100

    portal tests made

---
+++ src/unsw/dungeon/test/PortalSystemTest.java
@@ -0,0 +1,15 @@
+package unsw.dungeon.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import unsw.dungeon.*;
+
+class PortalSystemTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
