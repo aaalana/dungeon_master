3ed747cef129aaa37ae6968159304f0fcdaaaa7c
Alana Hua <z5209503@ad.unsw.edu.au>
2019-11-02 21:09:00 +1100

    added a key test for key.java

---
+++ src/unsw/dungeon/test/KeyTest.java
@@ -0,0 +1,20 @@
+package unsw.dungeon.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import unsw.dungeon.*;
+
+class KeyTest {
+
+	@Test
+	void testGetId() {
+		Key key = new Key(9, 2, 0);
+		assertEquals(0, key.getId());
+		Key key2 = new Key(9, 2, 1);
+		assertEquals(1, key2.getId());
+		Key key3 = new Key(9, 2, 3);
+		assertEquals(3, key3.getId());
+	}
+
+}
