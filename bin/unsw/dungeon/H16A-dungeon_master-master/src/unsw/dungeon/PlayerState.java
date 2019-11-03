41a2459e60ce44bdb4c52d86bf250476a255eead
Arthur Wong <z5205512@ad.unsw.edu.au>
2019-11-03 14:11:57 +1100

    Updated binary files

--- bin/unsw/dungeon/H16A-dungeon_master-master/src/unsw/dungeon/PlayerState.java
+++
@@ -1,7 +0,0 @@
-package unsw.dungeon;
-
-public interface PlayerState {
-	public void drinkInvincibilityPotion(Item potion);
-	public void expelInvincibilityPotion(Item potion);
-	public void killPlayer();
-}
