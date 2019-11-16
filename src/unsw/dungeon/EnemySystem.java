7fa9968a805ab84aa4497263505fc203891606a3
Arthur Wong <z5205512@ad.unsw.edu.au>
2019-11-16 20:11:29 +1100

    Fixed bug in enemySystem movement function

--- src/unsw/dungeon/EnemySystem.java
+++ src/unsw/dungeon/EnemySystem.java
@@ -43,49 +43,29 @@ public class EnemySystem implements Subject {
 			this.update();
 		}
 	}
-	
-<<<<<<< HEAD
-	 public void moveEnemies(int playerX, int playerY, boolean isInvincible) {
-	    	for (Entity entity : this.enemies) {
-	    		if (entity == null) continue;
-	    		
-	    		if (entity instanceof unsw.dungeon.Enemy) {
-	    			Enemy enemy = (Enemy) entity;
-	    			enemy.searchPlayer(playerX, playerY, isInvincible);
-	    		}
-	    	}
-	    	
-	    	for (Entity entity : this.enemies2) {
-	    		if (entity == null) continue;
-	    		
-	    		if (entity instanceof unsw.dungeon.Enemy2) {
-	    			Enemy2 enemy = (Enemy2) entity;
-	    			enemy.searchPlayer(playerX, playerY, isInvincible);
-	    		}
-	    	}
-	    }
-	 
-=======
-	/**
-	 * Moves the enemies in the dungeon
-	 * @param playerX
-	 * @param playerY
-	 */
-	public void moveEnemies(int playerX, int playerY) {
+
+
+	public void moveEnemies(int playerX, int playerY, boolean isInvincible) {
     	for (Entity entity : this.enemies) {
     		if (entity == null) continue;
     		
     		if (entity instanceof unsw.dungeon.Enemy) {
     			Enemy enemy = (Enemy) entity;
-    			enemy.searchPlayer(playerX, playerY);
+    			enemy.searchPlayer(playerX, playerY, isInvincible);
+    		}
+    	}
+    	
+    	for (Entity entity : this.enemies2) {
+    		if (entity == null) continue;
+    		
+    		if (entity instanceof unsw.dungeon.Enemy2) {
+    			Enemy2 enemy = (Enemy2) entity;
+    			enemy.searchPlayer(playerX, playerY, isInvincible);
     		}
     	}
     }
-	
-	/**
-	 * Updates the enemy goal
-	 */
->>>>>>> f82f22e8cafecf4969437c742fdc86a2e6944eca
+	 
+
 	public void update() {
 		System.out.println("All enemies are dead, updating goals");
 		enemyGoal.updateGoal();
